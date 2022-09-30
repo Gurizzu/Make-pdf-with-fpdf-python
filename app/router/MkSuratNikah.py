f225188b22775738e20a2981d99538d2ac64d4e3
EgaSepyana <egasepyana04@gmail.com>
2022-09-30 11:03:47 +0700

    add Logic for save pdf in local

--- app/router/MkSuratNikah.py
+++ app/router/MkSuratNikah.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, status
 from .. import schema , database
+from fpdf import FPDF
+from .. import func
 
 
 router = APIRouter(
@@ -7,6 +9,23 @@ router = APIRouter(
     tags=["SuratNikah"]
 )
 
+# Surat Nikah EndPoint
 @router.post("/")
 async def make_surat_Nikah(makesurat:schema.SuratNikahForm):
-    return await database.make_Nikah(makesurat.dict())
+    pdf = FPDF()
+    payload = await database.make_Nikah(makesurat.dict())
+    
+    save_download_path = "download/"
+    file_name = payload.get("_id")
+    saved_file_name = save_download_path + "pdf_" + file_name
+
+    footer_surat = {
+        "penanda_tangan_surat" : payload.get("penanda_tangan_surat"),
+        "nip_penandatangan_surat" : payload.get("nip_penandatangan_surat")
+    }
+
+    # await tm.run_nikah(data=payload, foot=footer_surat , output=saved_file_name)
+    
+    await func.set_nikah(fpdf=pdf , data=payload ,foot=footer_surat , output=saved_file_name)
+
+    return {"massage" : "Thanks for Download"}
